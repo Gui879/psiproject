8de996e94686e12f16a02775abd3f9699f61ff2e
Gui879 <gdocm2@gmail.com>
2018-01-07 04:22:59 +0000

    3rd attempt

--- server/database/database.js
+++ server/database/database.js
@@ -1,4 +1,4 @@
-var Sequelize = require('Sequelize');
+var Sequelize = require('sequelize');
 var projectDatabase = new Sequelize('novaims', 'novaims', 'Hj9p4DzP_W?x', {
   host: "den1.mssql1.gear.host",
     dialect:'mssql'
